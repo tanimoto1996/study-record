9a48ecd1d82f8e12101d88e33e109f50d7dbc4bc
tanimoto1996 <yumenikki2@gmail.com>
2021-07-18 22:02:12 +0900

    パスワード再設定画面追加。参考サイトを確認しながらロジック作成。動作確認済み

--- laravel/resources/views/auth/passwords/email.blade.php
+++ laravel/resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 
-@section('title', "ログイン")
+@section('title', "パスワード再設定")
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('assets/css/auth/login.css') }}">
@@ -12,29 +12,27 @@
   <!-- エラー表示 -->
   @include('components.error_list')
 
+  @if (session('status'))
+  <div class="card-text alert alert-success">
+    {{ session('status') }}
+  </div>
+  @endif
+
   <div class="login-container">
     <div id="output"></div>
     <div class="register-title">
-      ログイン
+      パスワード再設定
       <div class="login-logo">
         <img src="{{ asset('assets/image/login-logo.png') }}" alt="">
       </div>
     </div>
 
     <div class="form-box">
-      <form method="POST" action="{{ route('login') }}">
+      <form method="POST" action="{{ route('password.email') }}">
         @csrf
         <input name="email" type="email" placeholder="メールアドレス" value="{{ old('email') }}">
-        <input name="password" type="password" placeholder="パスワード" required>
-        <input type="hidden" type="remember" id="remember" value="on">
-
-        <a class="password-rest-link" href="{{ route('password.request') }}">パスワードを忘れた方はこちら</a>
-
-        <button class="btn btn-info btn-block login" type="submit">ログイン</button>
+        <button class="btn btn-info btn-block login" type="submit">メール送信</button>
       </form>
-    </div>
-    <div class="login-here">
-      <a href="{{ route('register') }}">アカウント作成</a>
     </div>
   </div>
 </div>
