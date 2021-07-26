b15c94a1fe149b3509101b19af039d35259ec033
tanimoto1996 <yumenikki2@gmail.com>
2021-07-26 11:09:14 +0900

    study_timeテーブル追加。トップページに学習時間の追加。

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/top';
 
     /**
      * Define your route model bindings, pattern filters, etc.
