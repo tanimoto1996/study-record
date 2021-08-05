5352395ec065641379b7a3f008c9d60812e3a3a5
tanimoto1996 <yumenikki2@gmail.com>
2021-08-05 23:46:01 +0900

    TODOテストコード（内容の変更と並び順）のテスト追加。

--- laravel/tests/Feature/TodoControllerTest.php
+++ laravel/tests/Feature/TodoControllerTest.php
@@ -56,4 +56,71 @@ class TodoControllerTest extends TestCase
         // POSTした「テスト」文字があればテスト成功
         $response->assertSee("テスト");
     }
+
+    /**
+     * 一覧画面にタスクが降順に表示されているか
+     * @test
+     */
+    public function testShowTaskDescendingOrder()
+    {
+        // userの情報を生成して取得
+        $user = factory(User::class)->create();
+        // ログイン済みにする
+        $this->actingAs($user);
+
+        $firstPost = factory(Todo::class)->create([
+            'todo_body' => 'first',
+            'todo_status' => '0',
+            'user_id' => $user->id,
+            'created_at' => '2021-07-20 10:30:00'
+        ]);
+
+        $secondPost = factory(Todo::class)->create([
+            'todo_body' => 'second',
+            'todo_status' => '1',
+            'user_id' => $user->id,
+            'created_at' => '2021-07-30 10:31:00'
+        ]);
+
+        $thirdPost = factory(Todo::class)->create([
+            'todo_body' => 'third',
+            'todo_status' => '0',
+            'user_id' => $user->id,
+            'created_at' => '2021-07-20 10:32:00'
+        ]);
+
+        // コントローラーで降順に並び替える
+        $response = $this->get(action('TodoController@showTodoList'));
+
+        // 降順で並んでいるかを確認する
+        $expects = array($secondPost->todo_body, $thirdPost->todo_body, $firstPost->todo_body);
+        $response->assertSeeInOrder($expects);
+    }
+
+    /**
+     * 投稿した内容を更新する
+     * @test
+     */
+    public function testPostUpdate()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $item = factory(Todo::class)->create([
+            'user_id' => $user->id,
+            'todo_body' => 'editTest',
+        ]);
+
+        factory(Todo::class)->create([
+            'user_id' => $user->id,
+            'todo_body' => 'notEditTest',
+        ]);
+        $task_id = $item->id;
+        $task_body = "変更しています";
+        $data = [$task_id => $item->id, $task_body => "変更しています"];
+
+        $this->get(action('TodoController@showTodoList', $data));
+        $this->assertDatabaseHas('todos', [
+            'todo_body' => '変更しています',
+        ]);
+    }
 }
